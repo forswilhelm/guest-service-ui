7611fe001cf22b50d1cca0349b21372e3fdaea65
Marcus Otterström <marcus.otterstrom@caspeco.se>
2024-10-22 17:29:00 +0200

    Fix lint issues

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -10,15 +10,15 @@ export default function Home() {
 			<Heading as="h1">Home page</Heading>
 			<Text color={ThemeColorVariable.OnBackgroundSubdued}>You made it!</Text>
 			<Alert type="info" marginTop={ThemeSpaceVariable.Medium}>
-				Join the "RMS - Frontend" channel on Mattermost if you haven't already!
+				Join the &quot;RMS - Frontend&quot; channel on Mattermost if you haven&apos;t already!
 			</Alert>
 			<Text marginTop={ThemeSpaceVariable.Medium} fontWeight="bold">
 				Next steps
 			</Text>
 			<List paddingLeft={ThemeSpaceVariable.Medium}>
-				<ListItem>1. Search for "TODO" in the source code and fix all occurrences</ListItem>
+				<ListItem>1. Search for &quot;TODO&quot; in the source code and fix all occurrences</ListItem>
 				<ListItem>
-					2. Generate TypeScript types and an API client from your backend's Swagger file (see other RMS
+					2. Generate TypeScript types and an API client from your backend&apos;s Swagger file (see other RMS
 					projects)
 				</ListItem>
 				<ListItem>3. Implement auth using the generated API client and SWR</ListItem>
