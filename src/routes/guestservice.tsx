90b0c46c944c66feac5ec4058c6ea1cec15cf1d3
Jörgen Karlsson <jorgen.karlsson@caspeco.se>
2025-02-19 12:39:43 +0100

    get guests

--- src/routes/guestservice.tsx
+++ src/routes/guestservice.tsx
@@ -1,38 +1,70 @@
+import { Api, GuestResponse } from "@/GuestApiTypes";
 import { Table, TableContent } from "@caspeco/casper-ui-library.components.table";
 import { Text } from "@caspeco/casper-ui-library.components.text";
-
-
+import { useEffect, useState } from "react";
 
 export default function GuestService() {
+
+  const api = new Api({baseUrl: "http://localhost:8083", baseApiParams: {
+    headers: {
+      "system": "erics-test-system",
+    },
+    format: "json",
+  }});
+
+
+
+        const [guests, setGuests] = useState<GuestResponse[]>([]);
+        
+        
+
+        useEffect(() => {
+
+          const getGuests = async () => {
+            try {
+              const response = await api.v1.getGuests();
+
+              setGuests(response.data.guestList);
+            } catch (error) {
+              console.error(error);
+            }
+          };
+
+          getGuests();
+
+        }, []); 
+
+        
+
         const columns = [
           {
             accessorKey: 'name',
             header: 'Namn',
           },
           {
-            accessorKey: 'id',
+            accessorKey: 'emailAddress',
             header: 'Email',
           },
         ];
       
-        const data = [
-          {
-            name: 'Wilhelm',
-            id: '1',
-          },
-          {
-            name: 'Jörgen',
-            id: '2',
-          },
-          {
-            name: 'Eric',
-            id: 3,
-          },
-        ];
+        // const data = [
+        //   {
+        //     name: 'Wilhelm',
+        //     identifier: '1',
+        //   },
+        //   {
+        //     name: 'Jörgen',
+        //     identifier: '2',
+        //   },
+        //   {
+        //     name: 'Eric',
+        //     id: 3,
+        //   },
+        // ];
 
 	return <>
     <Text>This is the guest service j</Text>
-    <Table data={data} columns={columns} localeString="en-US">
+    <Table data={guests} columns={columns} localeString="en-US">
     <TableContent />
     </Table>
     </>;
