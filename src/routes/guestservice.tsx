d088edda77d44dbb39ab122c3f4fccca6d928a36
Wilhelm Shakely Fors <wilhelm.fors@caspeco.se>
2025-02-19 13:41:15 +0100

    broken stuff

--- src/routes/guestservice.tsx
+++ src/routes/guestservice.tsx
@@ -1,71 +1,80 @@
 import { Api, GuestResponse } from "@/GuestApiTypes";
-import { Table, TableContent } from "@caspeco/casper-ui-library.components.table";
-import { Text } from "@caspeco/casper-ui-library.components.text";
+import { Box } from "@caspeco/casper-ui-library.components.box";
+import { Modal, ModalBody, ModalCloseButton, ModalHeader, useDisclosure } from "@caspeco/casper-ui-library.components.modal";
+import { Table, TableContent, TableDataType, TableToolbar } from "@caspeco/casper-ui-library.components.table";
+import { TableSubrows } from "@caspeco/casper-ui-library.components.table/dist/tableTypes";
 import { useEffect, useState } from "react";
 
 export default function GuestService() {
+	const api = new Api({
+		baseUrl: "http://localhost:8083",
+		baseApiParams: {
+			headers: {
+				system: "erics-test-system",
+			},
+			format: "json",
+		},
+	});
 
-  const api = new Api({baseUrl: "http://localhost:8083", baseApiParams: {
-    headers: {
-      "system": "erics-test-system",
-    },
-    format: "json",
-  }});
+	const [guests, setGuests] = useState<GuestResponse[]>([]);
+
+	useEffect(() => {
+		const getGuests = async () => {
+			try {
+				const response = await api.v1.getGuests();
+
+				setGuests(response.data.guestList);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		getGuests().catch(console.error);
+	}, []);
+
+	const columns = [
+		{
+			accessorKey: "name",
+			header: "Namn",
+		},
+		{
+			accessorKey: "emailAddress",
+			header: "Email",
+		},
+    {
+			accessorKey: "identifier",
+			header: "GuestID",
+		},
+	];
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [clickedGuestId, setClickedGuestId] = useState("");
 
 
+	return (
+  <>
+		<Box minWidth="600px">
+			<Table data={guests} columns={columns} localeString="en-US">
+      <TableToolbar
+        tableName="Guests"
+      />
+				<TableContent onClickRow={ setClickedGuestId } />
+			</Table>
+      </Box>
+      <GuestModal guestId={clickedGuestId} onClose={onClose} />
+    </>
+	);
+}
 
-        const [guests, setGuests] = useState<GuestResponse[]>([]);
-        
-        
-
-        useEffect(() => {
-
-          const getGuests = async () => {
-            try {
-              const response = await api.v1.getGuests();
-
-              setGuests(response.data.guestList);
-            } catch (error) {
-              console.error(error);
-            }
-          };
-
-          getGuests();
-
-        }, []); 
-
-        
-
-        const columns = [
-          {
-            accessorKey: 'name',
-            header: 'Namn',
-          },
-          {
-            accessorKey: 'emailAddress',
-            header: 'Email',
-          },
-        ];
-      
-        // const data = [
-        //   {
-        //     name: 'Wilhelm',
-        //     identifier: '1',
-        //   },
-        //   {
-        //     name: 'Jörgen',
-        //     identifier: '2',
-        //   },
-        //   {
-        //     name: 'Eric',
-        //     id: 3,
-        //   },
-        // ];
-
-	return <>
-    <Text>This is the guest service j</Text>
-    <Table data={guests} columns={columns} localeString="en-US">
-    <TableContent />
-    </Table>
-    </>;
+const GuestModal = ({guestId, onClose}:{guestId: string; onClose: () => void}) => {
+  return <Modal isOpen={guestId?.length > 0} onClose={onClose}>
+  <ModalHeader>
+    Modal title
+    <ModalCloseButton />
+  </ModalHeader>
+  <ModalBody>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur magnam.
+  </ModalBody>
+</Modal>
 }
